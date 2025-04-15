7e3045f928aa412ab790b1acb46a0c9af50b8431
Ahmed maher <ahmedofficial5036@gmail.com>
2025-04-15 20:15:30 +0200

    edited appbar

--- lib/resources/constants/app_images.dart
+++ lib/resources/constants/app_images.dart
@@ -1,5 +1,6 @@
 class AppImages {
   AppImages._();
   static const String profile = "assets/images/profile.jpeg";
-  static const String appLogo = "assets/images/app_logo.png";
+  //static const String appLogo = "assets/images/app_logo.png";
+  static const String appLogo = "assets/images/under_development.png";
 }
